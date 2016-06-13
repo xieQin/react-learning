00e1f27ab99fb5384fe5a512e3b5f82c5f41c35d
cherishhsieh <xieqin1991@gmail.com>
2016-06-13 11:18:01 +0800

    add todoList

---
+++ todoList/server.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var config = require('./webpack.config.js')
+var express = require('express')
+
+var app = new express()
+var port = 3000
+var compiler = webpack(config)
+
+app.use(webpackDevMiddleware(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath,
+  reload: true,
+  stats: {
+    colors: true,
+    chunks: false
+  }
+}))
+
+app.use(webpackHotMiddleware(compiler))
+
+app.use('/static', express.static('./static'))
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(port, function(error) {
+  if(error) {
+    console.log(error)
+  } else {
+    console.info(" ==> Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
